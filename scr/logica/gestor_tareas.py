74dc03b3345059a01bfdfad15356c5e8b5a3fb9d
yefer8 <75265472@continental.edu.pe>
2024-05-31 20:00:33 -0500

    Lanzamiento 1.1.0 correcto

--- scr/logica/gestor_tareas.py
+++ scr/logica/gestor_tareas.py
@@ -7,11 +7,30 @@ class Tarea:
 
 
 class GestorTareas:
-    def __init__(self):q
-        self.tareas = []
+     def __init__(self):q
+
+         self.tareas = []
 
     def agregar_tarea(self, titulo, descripcion):
         if not titulo:
             raise ValueError("El título no puede estar vacío")
         tarea = Tarea(titulo, descripcion)
         self.tareas.append(tarea)
+
+
+def ver_tareas(self):
+    return self.tareas
+
+ def marcar_completada(self, indice):
+        if 0 <= indice < len(self.tareas):
+            self.tareas[indice].completada = True
+        else:
+            raise IndexError("Índice fuera de rango")
+
+
+def eliminar_tarea(self, indice):
+    if 0 <= indice < len(self.tareas):
+        del self.tareas[indice]
+    else:
+        raise IndexError("Índice fuera de rango")
+
--- tests/test_gestor_tareas.py
+++ tests/test_gestor_tareas.py
@@ -14,3 +14,32 @@ class TestGestorTareas(unittest.TestCase):
     def test_agregar_tarea_sin_titulo(self):
         with self.assertRaises(ValueError):
             self.gestor.agregar_tarea("", "Descripción")
+
+
+    def test_ver_tareas(self):
+
+        self.gestor.agregar_tarea("Tarea 1", "Descripción de la tarea 1")
+        self.gestor.agregar_tarea("Tarea 2", "Descripción de la tarea 2")
+
+
+        tareas = self.gestor.ver_tareas()
+
+        self.assertNotEqual(len(tareas), 0)
+        self.assertEqual(tareas[0].titulo, "Tarea 1")
+        self.assertEqual(tareas[1].titulo, "Tarea 2")
+
+    def test_marcar_completada(self):
+
+        self.gestor.agregar_tarea("Tarea 1", "Descripción de la tarea 1")
+        self.gestor.marcar_completada(0)
+        self.assertTrue(self.gestor.tareas[0].completada)
+
+
+    def test_eliminar_tarea(self):
+
+        self.gestor.agregar_tarea("Tarea 1", "Descripción de la tarea 1")
+        self.gestor.eliminar_tarea(0)
+        self.assertEqual(len(self.gestor.tareas), 0)
+
+        if __name__ == "__main__":
+            unittest.main()
